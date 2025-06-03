310b51f81f62a628ae78a6963720c0c75707d976
sankalpg0 <sankalpg02@gmail.com>
2025-06-03 17:18:08 +0530

    changes

--- calculator.sh
+++ calculator.sh
@@ -6,7 +6,7 @@ while true; do
 echo "---- Menu ----"
 echo "0. Exit"
 echo "--------------"
-echo "SANKALP did this"
+echo "SANKALP did this but navnath is bad collaborator"
 read -p "Enter choice: " choice
 case $choice in
     0) echo "Exiting..."
