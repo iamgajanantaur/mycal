4d85274d9e2c6c1bbc9dcdf96261389760b9e5ef
Gajanan Taur <Gajanan Taur>
2025-06-03 16:54:36 +0530

    basic flow added

---
+++ calculator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+while true; do
+echo "---- Menu ----"
+echo "0. Exit"
+echo "--------------"
+
+read -p "Enter choice: " choice
+case $choice in
+    0) echo "Exiting..."
+       exit 0
+       ;;
+    
+    
+esac
+done
